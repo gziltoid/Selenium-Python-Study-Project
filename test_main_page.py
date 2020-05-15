e823c1cd1af64b206435ad52cf08a47055c67b72
gziltoid <g.ziltoid@gmail.com>
2020-05-15 00:44:34 -0700

    Implement page object model

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,10 @@
+from Selenium_Python_Study_Project.pages.main_page import MainPage
+
 link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
