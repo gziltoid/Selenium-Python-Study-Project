321bb1ebf5e777e7e11391ec93915a7bdce94446
gziltoid <g.ziltoid@gmail.com>
2020-05-16 22:42:00 -0700

    Add method is_element_present() to class BasePage and check #login_link presence on main page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,4 @@
-from Selenium_Python_Study_Project.pages.main_page import MainPage
+from .pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -7,4 +7,8 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
 
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
