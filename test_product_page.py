b0385ec40efa20734513c0e486c6a8d5c51d302f
gziltoid <g.ziltoid@gmail.com>
2020-05-26 18:33:19 -0700

    Add product link parameterization to test_guest_can_add_product_to_card()

--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,28 @@
+import pytest
+
 from .pages.product_page import ProductPage
-import time
+#
+# links1 = ["http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear",
+#           "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"]
 
-link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+links = [
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                 marks=pytest.mark.xfail),
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+]
 
 
-def test_guest_can_add_product_to_card(browser):
-    product_page = ProductPage(browser, link)
+@pytest.mark.parametrize('product_link', links)
+def test_guest_can_add_product_to_card(browser, product_link):
+    product_page = ProductPage(browser, product_link)
     product_page.open()
     product_page.should_be_add_to_card_button()
     product_page.add_item_to_card()
