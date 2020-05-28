de58b33d5192bbadf254b54df59efc6efc922ab4
gziltoid <g.ziltoid@gmail.com>
2020-05-28 13:34:37 -0700

    Move go_to_login_page() and should_be_login_link() from class MainPage to BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,6 +4,7 @@ from selenium.webdriver import Remote as RemoteWebDriver
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, NoAlertPresentException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 
 
 class BasePage:
@@ -14,6 +15,13 @@ class BasePage:
 
     def open(self):
         self.browser.get(self.url)
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not present."
 
     def is_element_present(self, how, what):
         try:
@@ -48,4 +56,4 @@ class BasePage:
             print(f"Your code: {alert_text}")
             alert.accept()
         except NoAlertPresentException:
-            print("No second alert presented.")
+            print("No second alert present.")
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,8 @@
 import pytest
 
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
+
 #
 # links1 = ["http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear",
 #           "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"]
@@ -22,6 +24,22 @@ links = [
 ]
 
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+
 @pytest.mark.parametrize('product_link', links)
 @pytest.mark.skip
 def test_guest_can_add_product_to_cart(browser, product_link):
@@ -33,22 +51,26 @@ def test_guest_can_add_product_to_cart(browser, product_link):
     product_page.should_have_correct_item_title_in_message()
     product_page.should_have_correct_total_price_in_message()
 
+
 @pytest.mark.xfail
+@pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
     product_page = ProductPage(browser, base_link)
     product_page.open()
     product_page.add_item_to_cart()
     product_page.should_not_have_success_message()
 
+@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     product_page = ProductPage(browser, base_link)
     product_page.open()
     product_page.should_not_have_success_message()
 
+
 @pytest.mark.xfail
+@pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_cart(browser):
     product_page = ProductPage(browser, base_link)
     product_page.open()
     product_page.add_item_to_cart()
     product_page.should_disappear_success_message()
-
